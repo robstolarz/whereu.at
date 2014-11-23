9ab56d408fd4f8c557f6de17abc58fc08a2b0943
Robert Stolarz <robert.stolarz@yandex.com>
2014-11-23 18:07:12 -0500

    Update schema to fit Docs

--- server/api/at/at.model.js
+++ server/api/at/at.model.js
@@ -1,25 +1,55 @@
 'use strict';
 
+require('es6-shim');
+
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema,
     ObjectId = mongoose.Schema.ObjectId,
     Mixed = mongoose.Schema.Mixed;
-
-var AtSchema = new Schema({
-  accepted:{
+var positionInterface = {
+  coords: {
     latitude:Number,
     longitude:Number,
-    altitude:Number,
-    accuracy:Number
+    accuracy:Number, 
+    altitude:Number, 
+    altitudeAccuracy:Number, 
+    heading:Number, 
+    speed:Number
   },
-  from:{type:ObjectId,ref:'User'},
-  to:{type:ObjectId,ref:'User'},
-  time:Date,
-  viewed:Boolean,
-  message:{
-    default:"where u @?",
-    type:String
-  }
+  timestamp:Date
+};
+
+var AtSchema = new Schema({
+  asker:{
+    user:{type:ObjectId,ref:'User'},
+    location:positionInterface
+  },
+  answerer:{
+    user:{type:ObjectId,ref:'User'},
+    location:positionInterface
+  },
+  when:Date
+});
+
+AtSchema.static('areFriends',function(u1,u2){
+  return At.aggregate([
+    {$match:{
+        $and:[
+          {$or:[
+            {asker:{user:u1},answerer:{user:u2}},
+            {asker:{user:u2},answerer:{user:u1}},
+          ]},
+          {answerer:{location:{timestamp:{$exists:true}}}}
+        ]
+      }
+    },
+    {$limit:1}
+  ]);
+});
+
+AtSchema.pre('save', function (next) {
+  this.when = Math.max(this.asker.location.timestamp||0,this.answerer.location.timestamp||0);
+  next();
 });
 
 module.exports = mongoose.model('At', AtSchema);
